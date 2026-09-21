8427820a8469d393a5fda4848fd1a619085cb993
sanghun17 <sanghun17@users.noreply.github.com>
2026-09-22 01:58:06 +0900

    Pin continuous prearm stream fix

--- deployment/stack-modules/control/flight-safety/clone.sh
+++ deployment/stack-modules/control/flight-safety/clone.sh
@@ -6,5 +6,5 @@ set -e
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/../../.." && pwd)"
 DST="$ROOT/ws/flight-safety/src/flight_safety"
 REPO="${SAFETY_REPO:-git@github.com:sanghun17/flight_safety.git}"
-BRANCH="${SAFETY_REVISION:-7257d68d575c82c3cc6e32baa8db07dcaa7cca72}"
+BRANCH="${SAFETY_REVISION:-c3ae14accbd9f0796b8840deffc4c93e1b03f34c}"
 bash "$ROOT/scripts/lib/clone_repo.sh" "$DST" "$REPO" "$BRANCH"
